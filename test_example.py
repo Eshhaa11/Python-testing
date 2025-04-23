d9d826c4cd0c8c7b43d36e3cbd6655ab67e5d6f4
Esha Patel <Patellesha11@gmail.com>
2025-04-23 17:08:49 +0300

    added

--- test_example.py
+++ test_example.py
@@ -20,4 +20,26 @@ def test_sub() :
 def sub(a,b):
     return a - b
 
+def test_multiplication() :
+
+    assert multiplication(2, 2) == 4
+
+    assert multiplication(10, 4) == 40
+
+    assert multiplication(15, 2) == 30
+
+def multiplication(a,b):
+    return a * b
+
+
+def test_division() :
+    assert division(10, 4) == 2.5
+
+    assert division(25, 5) == 5
+
+    assert division(28, 2) == 14
+
+def division(a,b):
+    return a / b
+
 
