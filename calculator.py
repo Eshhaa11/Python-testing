cf9db9ae2f16f8dfc04f59761dc0fca8fc18cc50
Esha Patel <Patellesha11@gmail.com>
2025-04-23 17:27:29 +0300

    added

--- calculator.py
+++ calculator.py
@@ -3,14 +3,14 @@ class Calculator:
     def add(self , a, b):
         return a + b
     
-    def sub(a,b):
+    def sub(self, a,b):
         return a - b
     
-    def multiplication(a,b):
+    def multiplication(self, a,b):
         return a * b
     
     
-    def division(a,b):
+    def division(self, a,b):
         return a / b
 
 
